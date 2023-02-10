24d9218751197c50f56e8d486cded7a23cd1c307
Elmir <elmir.quluzade627@gmail.com>
2023-02-10 10:37:28 +0400

    done

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,30 +1,41 @@
 const Product = require('../model/product')
 
 exports.allProducts = async (req, res) => {
-    const products = Product.find()
-    // !Searching
-    if (req.query.search) {
-        const pattern = `${req.query.search}`
-        products.find({ title: { $regex: pattern, $options: "i" } })
-    }
-    // !Filtering
-    const queryStr = { ...req.query };
-    const restrictedFields = ["sort", "page", "limit", "fields", "size", "search"];
-    restrictedFields.forEach((f) => delete queryStr[f]);
-    let tempoQueryStr = JSON.stringify(queryStr).replace(
-        /\b(gt|gte|lt|lte)\b/g,
-        (atomic) => `$${atomic}`
-    );
-    products.find(JSON.parse(tempoQueryStr));
-    if (req.query.size) {
-        let sizes = req.query.size.split(',')
-        products.find({ availableSizes: { $all: sizes } })
-    }
+    try {
+        const products = Product.find()
+        // !Searching
+        if (req.query.search) {
+            const pattern = `${req.query.search}`
+            products.find({ title: { $regex: pattern, $options: "i" } })
+        }
+        // !Filtering
+        const queryStr = { ...req.query };
+        const restrictedFields = ["sort", "page", "limit", "size", "search"];
+        restrictedFields.forEach((f) => delete queryStr[f]);
+        let tempoQueryStr = JSON.stringify(queryStr).replace(
+            /\b(gt|gte|lt|lte)\b/g,
+            (atomic) => `$${atomic}`
+        );
+        products.find(JSON.parse(tempoQueryStr));
+        if (req.query.size) {
+            let sizes = req.query.size.split(',')
+            products.find({ availableSizes: { $all: sizes } })
+        }
 
-    const allData = await products
-    res.json({
-        success: true,
-        quantity: allData.length,
-        allData
-    })
+        const allData = await products
+        res.json({
+            success: true,
+            quantity: allData.length,
+            allData
+        })
+    } catch (error) {
+        res.json({
+            success: false,
+            message: error.message
+        })
+    }
+}
+
+exports.createProduct = async (req, res) => {
+
 }
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 const productController = require('../controllers/productController')
 
 router.get('/', productController.allProducts)
+router.post('/', productController.createProduct)
 
 module.exports = router
 
